47ef34d82b633dad36eac9f10389ad4c8123ccac
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2023-09-28 19:56:22 +0200

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,12 @@
+namespace DartlingGunnerFourthPath;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "38.3";
+    public const string Version = "1.0.0";
+    public const string Name = "The Nine Path Dartling Gunner";
+    public const string Description = "REQUIRE path plus plus";
+    public const string RepoOwner = "Mattcy1";
+    public const string RepoName = "The Nine Path Dartling Gunner";
+    public const string Icon = "Icon.png";
+}
